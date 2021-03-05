d97b40c732204bd5c464c22c4240858307c2c029
Adrian-Coder97 <adrian_lch@outlook.com>
2021-03-05 01:40:23 -0600

    final

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-const nombreProducto = document.querySelector('#nombre');
-const precioProducto = document.querySelector('#precio');
-const btnGuardar = document.querySelector('#guardar');
-const btnLimpiar = document.querySelector('#limpiar');
-const listaProducto = document.querySelector('#lista');
+const productName = document.querySelector("#productName");
+const productPrice = document.querySelector("#productPrice");
+const buttonSave = document.querySelector("#button-save");
+const buttonCancel = document.querySelector("#button-cancel");
+const productList = document.querySelector("#product-list");
+const totalOutput = document.querySelector("#total");
 
-const nuevo = (name, price) => {
-    const ionCard = document.createElement('ion-card');
-    const nuevoItem = document.createElement('ion-card-content');
-    nuevoItem.textContent = name + ': $'+ price;
-    ionCard.appendChild(nuevoItem);
-    listaProducto.appendChild(ionCard);
-}
+total = 0;
 
-btnGuardar.addEventListener('click', () => {
-    const name = nombreProducto.value;
-    const price = precioProducto.value;
-    console.log("Nombre = " + name + " Price = " + price);
-    nuevo(name, price); 
+const createNewProduct = (name, price) => {
+  const ionCard = document.createElement('ion-card');
+  const newProductItem = document.createElement("ion-card-content");
+  ionCard.appendChild(newProductItem)
+  newProductItem.textContent = name + ": $" + price;
+  productList.appendChild(ionCard);
+};
+
+const clearInputs = () => {
+  productName.value = "";
+  productPrice.value = "";
+};
+
+const presentAlert = () => {
+  const alert = document.createElement("ion-alert");
+  alert.header = "Invalid Data";
+  alert.subHeader = "Please verify your input";
+  alert.message = "Incorrect Name or Price";
+  alert.buttons = ["OK"];
+
+  document.body.appendChild(alert);
+  return alert.present();
+};
+
+const isEmpty = str => !str.trim().length;
+
+buttonCancel.addEventListener("click", clearInputs);
+
+buttonSave.addEventListener("click", async () => {
+  const name = productName.value;
+  const price = productPrice.value;
+
+  if (isEmpty(name) || price <= 0 || isEmpty(price)) {
+    presentAlert();
+    return;
+  }
+
+  createNewProduct(name, price);
+  total += +price;
+  totalOutput.textContent = total;
+  clearInputs();
 });
